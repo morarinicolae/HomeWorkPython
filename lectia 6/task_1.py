b5a629b00d29c946b2283205df576df4c442db6d
Morari Nicolae <42006679+morarinicolae@users.noreply.github.com>
2024-04-17 15:16:01 +0000

    update tema 6 si 7

--- lectia 6/task_1.py
+++ lectia 6/task_1.py
@@ -3,35 +3,46 @@
 # Creați un dicțioar gol
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar = {}
 # CODUL TĂU VINE MAI SUS:
 
 # Acum adăugați o pereche de cheie-valoare în dicționar, cheia fiind "name" și valoarea fiind "John"
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar = {
+"name" : "John"
+}
 # CODUL TĂU VINE MAI SUS:
 
 # Acum adăugați o pereche de cheie-valoare în dicționar, cheia fiind "age" și valoarea fiind 30
 
 # CODUL TĂU VINE MAI JOS:
+dictionar = {
+  "name" : "John"
+}
+dictionar.update({"age" : 30})
 
+# or
+dictionar = {
+  "name" : "John"
+}
+dictionar ["age"] = 30
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișați dicționarul
 
 # CODUL TĂU VINE MAI JOS:
-
+print(dictionar)
 # CODUL TĂU VINE MAI SUS:
 
 # Acum ștergeți cheia "name" din dicționar
 
 # CODUL TĂU VINE MAI JOS:
-
+del dictionar["name"]
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișați dicționarul
-
+print(dictionar)
 # CODUL TĂU VINE MAI JOS:
 
 # CODUL TĂU VINE MAI SUS:
@@ -39,79 +50,79 @@
 # Acum adăugați o pereche de cheie-valoare în dicționar, cheia fiind "city" și valoarea fiind "New York" folosind metoda setdefault()
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar.setdefault("city", "New York")
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați toate cheile din dicționar 
 
 # CODUL TĂU VINE MAI JOS:
-
+print(dictionar.keys())
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați toate valorile din dicționar
 
 # CODUL TĂU VINE MAI JOS:
-
+print(dictionar.values())
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați toate perechile de cheie-valoare din dicționar folosind metoda items()
 
 # CODUL TĂU VINE MAI JOS:
-
+print(dictionar.items())
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați numărul de perechi de cheie-valoare din dicționar
 
 # CODUL TĂU VINE MAI JOS:
-
+print(len(dictionar))
 # CODUL TĂU VINE MAI SUS:
 
 # Extrageți valoarea unei chei inexistente fără a genera o eroare
 
 # CODUL TĂU VINE MAI JOS:
-
+print(dictionar.get("name"))
 # CODUL TĂU VINE MAI SUS:
 
 # Acum actualizați dicționarul cu un alt dicționar, folosind metoda update()
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar.update({"name" : "Ion", "age" : 33, "city" : "New Mexico"})
 # CODUL TĂU VINE MAI SUS:
 
 # Setați valoarea cheii "pizza" la 10 folosind metoda setdefault()
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar.setdefault("pizza", 10)
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați dicționarul
 
 # CODUL TĂU VINE MAI JOS:
-
+print (dictionar)
 # CODUL TĂU VINE MAI SUS:
 
 # Ștergeți cheia "pizza" din dicționar folosind metoda pop()
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar.pop("pizza")
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați dicționarul
 
 # CODUL TĂU VINE MAI JOS:
-
+print (dictionar)
 # CODUL TĂU VINE MAI SUS:
 
 # Ștergeți toate perechile de cheie-valoare din dicționar
 
 # CODUL TĂU VINE MAI JOS:
-
+dictionar.clear()
 # CODUL TĂU VINE MAI SUS:
 
 # Afișați dicționarul
 
 # CODUL TĂU VINE MAI JOS:
-
+print (dictionar)
 # CODUL TĂU VINE MAI SUS:
 
 # Asta a fost tot, ai terminat prima ta sarcină legată de dicționare
--- lectia 7/task_1.py
+++ lectia 7/task_1.py
@@ -3,43 +3,58 @@
 # Creați o variabilă numită number și atribuiți-i o valoare întreagă.
 
 # CODUL TĂU VINE MAI JOS:
-
+number = 5
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este pozitiv și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number > 0:
+  print("este pozitiv")
+else:
+  print("este negativ")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este par și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number % 2 == 0:
+  print("este par")
+else:
+  print("este impar")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este impar și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number % 2 != 0:
+  print("este impar")
+else:
+  print("este par")
 # CODUL TĂU VINE MAI SUS:
 
 # Creați o variabilă text și atribuiți-i un șir de caractere.
 
 # CODUL TĂU VINE MAI JOS:
-
+text = "un sir de caractere"
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "Python" in text:
+  print("textul contine cuvantul Python")
+else:
+  print("textul nu contine cuvantul Python")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Java" și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "Java" in text:
+  print("textul contine cuvantul Java")
+else:
+  print("textul nu contine cuvantul Java")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
@@ -47,7 +62,12 @@
 # Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "Python" in text:
+  print("textul contine cuvântul Python")
+elif "Java" in text:
+  print("textul contine cuvântul Java")
+else:
+  print("textul nu contine cuvantul Python si nici java")
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și cuvântul "Java" și afișați un mesaj corespunzător.
@@ -55,7 +75,12 @@
 # Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "Python" in text and "Java" in text:
+  print ("textul contine cuvantul Python si cuvantul Java")
+elif "Python" in text or "Java" in text:
+  print("textul contine cuvantul Python sau cuvantul Java")
+else:
+  print("textul nu contine Java sau Python")
 # CODUL TĂU VINE MAI SUS:
 
 # Extrageți de la tastatură utilizând funcția input un număr de la 1 la 5 și atribuiți-l variabilei number.
@@ -67,7 +92,19 @@
 # pentru 5 - printați "Cinci"
 
 # CODUL TĂU VINE MAI JOS:
-
+number = int(input("introduceti un numar de la 1 la 5: "))
+if number == 1:
+  print("Unu")
+elif number == 2:
+  print("Doi")
+elif number == 3:
+  print("Trei")
+elif number == 4:
+  print("Patru")
+elif number == 5:
+  print("Cinci")
+else:
+  print("trebuie sa introduceti doar un numar de la 1 la 5")
 # CODUL TĂU VINE MAI SUS:
 
 
